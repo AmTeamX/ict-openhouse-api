4729aed2ffaee96938f4ee28cf37cd303f79a3fb
Teamm <amteamxmail@gmail.com>
2025-09-02 22:35:03 +0700

    delete error

--- src/route/quests/rewards/helper/getRewardEligibility.ts
+++
@@ -1,7 +0,0 @@
-import {
-  IQuestStatus,
-  QUEST_STATUS,
-} from '~/routes/quests/helpers/getQuestStatus'
-
-export const getRewardEligibility = async (questStatus: IQuestStatus[]) =>
-  questStatus.every((q) => q.status === QUEST_STATUS.SUCCESS_QUEST)
